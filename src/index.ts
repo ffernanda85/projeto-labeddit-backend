2d18b6e88c0cb432947700bd4984e35732964679
ffernanda85 <f.fernanda85@gmail.com>
2023-09-05 21:37:35 -0300

    feat: basic installation

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(process.env.PORT || 3003, () => {
+    console.log(`Servidor rodando na porta ${3003}`)
+})
